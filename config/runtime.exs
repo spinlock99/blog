796858b2d54cf522ecf0afb5adc9fb5e21a4fd11
Andrew Dixon <dixon@largearcade.com>
2024-10-11 10:55:31 -0700

    Configure Runtime
    - Enable SSL
    - Set Server, HTTP, and Host for Gigalixir

--- config/runtime.exs
+++ config/runtime.exs
@@ -31,7 +31,7 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :blog, Blog.Repo,
-    # ssl: true,
+    ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
@@ -54,14 +54,15 @@ if config_env() == :prod do
   config :blog, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :blog, BlogWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    server: true,
+    url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      port: {:system, "PORT"}
     ],
     secret_key_base: secret_key_base
 
